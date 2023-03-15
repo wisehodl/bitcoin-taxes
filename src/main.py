09607260b9dfefb07333e8892ce42927d0f736a2
Jay <wisehodl@pm.me>
2023-03-15 17:39:45 -0400

    Wrote functions to read in transaction history from Gemini.

---
+++ src/main.py
@@ -0,0 +1,111 @@
+"""Bitcoin capital gains calculator."""
+
+from datetime import datetime
+from decimal import Decimal
+from pathlib import Path
+
+import pandas
+from pandas import DataFrame
+
+
+class Transaction:
+    """A BTCUSD transaction."""
+
+    def __init__(self, timestamp: datetime, btc: float, usd: float):
+        self._timestamp = timestamp
+        self._btc = Decimal(str(btc))
+        self._usd = Decimal(str(usd))
+
+    @property
+    def timestamp(self):
+        """Returns the transaction timestamp."""
+        return self._timestamp
+
+    @property
+    def btc(self):
+        """Returns the transactions BTC amount."""
+        return self._btc
+
+    @property
+    def usd(self):
+        """Returns the transactions USD amount."""
+        return self._usd
+
+    def __eq__(self, other: "Transaction"):
+        if self is other:
+            return True
+
+        return (
+            self.timestamp == other.timestamp
+            and self.btc == other.btc
+            and self.usd == other.usd
+        )
+
+    def __repr__(self):
+        classname = type(self).__name__
+        timestamp = f"timestamp='{self.timestamp.isoformat()}'"
+        btc = f"btc={self.btc}"
+        usd = f"usd={self.usd}"
+
+        return f"{classname}({timestamp}, {btc}, {usd})"
+
+
+class Buy(Transaction):
+    """A BTCUSD buy transaction."""
+
+    def __init__(self, timestamp, btc, usd):
+        if btc < 0:
+            raise ValueError("BTC value must be positive for a buy.")
+        if usd > 0:
+            raise ValueError("USD value must be negative for a buy.")
+
+        super().__init__(timestamp, btc, usd)
+
+
+class Sell(Transaction):
+    """A BTCUSD sell transaction."""
+
+    def __init__(self, timestamp, btc, usd):
+        if btc > 0:
+            raise ValueError("BTC value must be negative for a sell.")
+        if usd < 0:
+            raise ValueError("USD value must be positive for a sell.")
+
+        super().__init__(timestamp, btc, usd)
+
+
+def read_gemini_input(path: Path) -> DataFrame:
+    """Returns a transaction dataframe from a Gemini transaction history."""
+
+    columns = {
+        "Date": "timestamp",
+        "Type": "type",
+        "USD Amount USD": "usd",
+        "BTC Amount BTC": "btc",
+    }
+    tx_types = ("Buy", "Sell")
+
+    dataframe = pandas.read_excel(path, usecols=columns.keys())
+    dataframe = dataframe.rename(columns=columns)
+    dataframe = dataframe.loc[dataframe["type"].isin(tx_types)]
+    dataframe = dataframe.sort_values(by=["timestamp"])
+
+    return dataframe
+
+
+def get_buys(input_df: DataFrame):
+    """Returns a list of buy transactions from the input dataframe."""
+    return [
+        Buy(r.timestamp, r.btc, r.usd)
+        for r in input_df.itertuples()
+        if r.type == "Buy"
+    ]
+
+
+def get_sells(input_df: DataFrame):
+    """Returns a list of sell transactions from the input dataframe."""
+    return [
+        Sell(r.timestamp, r.btc, r.usd)
+        for r in input_df.itertuples()
+        if r.type == "Sell"
+    ]
---
+++ tests/test_main.py
@@ -0,0 +1,118 @@
+"""Tests for bitcoin capital gains calculator."""
+
+import shutil
+from datetime import datetime
+from decimal import Decimal
+from pathlib import Path
+
+import pytest
+from pandas import DataFrame
+
+from main import Buy, Sell, Transaction, get_buys, get_sells, read_gemini_input
+
+TEST_INPUTS_PATH = Path("tests/input/")
+
+
+def test_tx_decimal():
+    """A transaction should hold amounts as Decimal objects."""
+
+    txn = Transaction(datetime.now(), 1.23, 2.34)
+    assert txn.btc == Decimal("1.23")
+    assert txn.usd == Decimal("2.34")
+
+
+def test_sell_tx():
+    """A sell transaction can only have a positive usd and negative btc."""
+
+    with pytest.raises(ValueError, match="USD"):
+        Sell(datetime.now(), -1, -1)
+
+    with pytest.raises(ValueError, match="BTC"):
+        Sell(datetime.now(), 1, 1)
+
+
+def test_buy_tx():
+    """A buy transaction can only have a positive usd and negative btc."""
+
+    with pytest.raises(ValueError, match="BTC"):
+        Buy(datetime.now(), -1, -1)
+
+    with pytest.raises(ValueError, match="USD"):
+        Buy(datetime.now(), 1, 1)
+
+
+@pytest.fixture(scope="function", name="gemini_input")
+def gemini_input_fixture(tmp_path: Path) -> Path:
+    """Provides a minimal gemini transaction history file path."""
+
+    filename = "gemini.xlsx"
+    shutil.copy2(TEST_INPUTS_PATH / filename, tmp_path)
+
+    return tmp_path / filename
+
+
+@pytest.fixture(name="gemini_input_df")
+def gemini_input_dataframe_fixture():
+    """Returns a sample transaction input dataframe."""
+
+    return DataFrame(
+        data={
+            "timestamp": [
+                datetime(2020, 6, 23, 20, 42, 26, 889000),
+                datetime(2020, 6, 23, 20, 45, 3, 979000),
+                datetime(2020, 6, 24, 16, 13, 54),
+                datetime(2020, 8, 17, 14, 13, 24, 948000),
+                datetime(2021, 6, 8, 15, 4, 56, 840000),
+            ],
+            "type": ["Buy", "Sell", "Buy", "Sell", "Sell"],
+            "usd": [-5, 5, -10, 10, 15],
+            "btc": [1, -1, 2, -2, -3],
+        }
+    )
+
+
+@pytest.fixture(name="gemini_buys")
+def gemini_buys_fixture():
+    """Returns the buys from the gemini transaction input."""
+
+    return [
+        Buy(datetime(2020, 6, 23, 20, 42, 26, 889000), 1, -5),
+        Buy(datetime(2020, 6, 24, 16, 13, 54), 2, -10),
+    ]
+
+
+@pytest.fixture(name="gemini_sells")
+def gemini_sells_fixture():
+    """Returns the sells from the gemini transaction input."""
+
+    return [
+        Sell(datetime(2020, 6, 23, 20, 45, 3, 979000), -1, 5),
+        Sell(datetime(2020, 8, 17, 14, 13, 24, 948000), -2, 10),
+        Sell(datetime(2021, 6, 8, 15, 4, 56, 840000), -3, 15),
+    ]
+
+
+def test_read_gemini_input(gemini_input, gemini_input_df):
+    """Should read Gemini transaction history into buys and sells."""
+
+    dataframe = read_gemini_input(gemini_input)
+    expected = gemini_input_df
+
+    assert dataframe["timestamp"].tolist() == expected["timestamp"].tolist()
+    assert dataframe["type"].tolist() == expected["type"].tolist()
+    assert dataframe["usd"].tolist() == expected["usd"].tolist()
+    assert dataframe["btc"].tolist() == expected["btc"].tolist()
+
+
+def test_get_buys(gemini_input_df, gemini_buys):
+    """Should extract buy transactions from the input dataframe."""
+
+    buys = get_buys(gemini_input_df)
+    assert buys == gemini_buys
+
+
+def test_get_sells(gemini_input_df, gemini_sells):
+    """Should extract sell transactions from the input dataframe."""
+
+    sells = get_sells(gemini_input_df)
+    assert sells == gemini_sells
