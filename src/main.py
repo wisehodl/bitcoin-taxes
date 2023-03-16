5c5d00cb0af22817a1c09feec68342518a81a350
Jay <wisehodl@pm.me>
2023-03-16 16:55:13 -0400

    Finished, but there is a bug where some sales are selling future buys.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 """Bitcoin capital gains calculator."""
 
+import argparse
+import csv
 from datetime import datetime
 from decimal import Decimal
+from enum import Enum
 from pathlib import Path
+from pprint import pprint
 
 import pandas
 from pandas import DataFrame
@@ -34,7 +38,19 @@ class Transaction:
     @property
     def price(self) -> Decimal:
         """Returns the BTCUSD price of the transaction."""
-        return abs(self.usd / self.btc)
+        return self.usd / self.btc
+
+    def split(self, split_btc: Decimal) -> tuple["Transaction", "Transaction"]:
+        """Splits the transaction at the btc amount and returns two new
+        Transaction objects.
+        """
+        split_usd = split_btc * self.price
+        remainder_btc = self.btc - split_btc
+        remainder_usd = self.usd - split_usd
+
+        return type(self)(self.timestamp, split_btc, split_usd), type(self)(
+            self.timestamp, remainder_btc, remainder_usd
+        )
 
     def __eq__(self, other: "Transaction"):
         if self is other:
@@ -77,6 +93,64 @@ class Sell(Transaction):
             raise ValueError("USD value must be positive for a sell.")
 
         super().__init__(timestamp, btc, usd)
+
+
+class Duration(Enum):
+    """Duration for a capital gain."""
+
+    SHORT = "Short"
+    LONG = "Long"
+
+
+class CapitalGain:
+    """A buy/sell transaction pair for capital gains calculation."""
+
+    def __init__(self, buy: Buy, sell: Sell):
+        self._buy = buy
+        self._sell = sell
+
+    @property
+    def buy(self):
+        return self._buy
+
+    @property
+    def sell(self):
+        return self._sell
+
+    @property
+    def duration(self):
+        """Returns the duration category of the capital gain."""
+
+        sts = self.sell.timestamp
+        adjusted_sell_timestamp = datetime(
+            sts.year - 1,
+            sts.month,
+            sts.day,
+            sts.hour,
+            sts.minute,
+            sts.second,
+            sts.microsecond,
+            tzinfo=sts.tzinfo,
+        )
+
+        if self.buy.timestamp < adjusted_sell_timestamp:
+            return Duration.LONG
+
+        return Duration.SHORT
+
+    @property
+    def gain(self):
+        """Returns the net profit (or loss) of the capital gain."""
+        return self.sell.usd + self.buy.usd
+
+    def __eq__(self, other: "CapitalGain"):
+        if self is other:
+            return True
+
+        return self.buy == other.buy and self.sell == other.sell
+
+    def __repr__(self):
+        return f"CapitalGain({self.buy}, {self.sell})"
 
 
 def read_gemini_input(path: Path) -> DataFrame:
@@ -127,7 +201,157 @@ def change_strategy(buys: list[Buy], strategy: str):
         buys.sort(key=lambda b: b.timestamp, reverse=False)
 
     elif strategy == "most_expensive_first_out":
-        buys.sort(key=lambda b: b.price, reverse=False)
+        buys.sort(key=lambda b: abs(b.price), reverse=False)
 
     elif strategy == "least_expensive_first_out":
-        buys.sort(key=lambda b: b.price, reverse=True)
+        buys.sort(key=lambda b: abs(b.price), reverse=True)
+
+    else:
+        raise ValueError(f"Unknown strategy: '{strategy}'")
+
+
+def pop_buys(buys: list[Buy], timestamp: datetime, btc: Decimal):
+    """Pops buys from the buy list up to the btc amount."""
+
+    popped = []
+
+    while btc:
+        pbuy = buys.pop()
+
+        if btc >= pbuy.btc:
+            popped.append(pbuy)
+            btc -= pbuy.btc
+
+        elif btc < pbuy.btc:
+            split, remainder = pbuy.split(btc)
+            popped.append(split)
+            buys.append(remainder)
+            btc -= split.btc
+
+    return popped
+
+
+def split_sell(sell: Sell, buys: list[Buy]):
+    """Splits a sell transaction according to its matched buys."""
+
+    assert sell.btc == -sum(b.btc for b in buys), "BTC amount must match."
+
+    sells = []
+
+    for buy in buys:
+        split, sell = sell.split(-buy.btc)
+        sells.append(split)
+
+    return sells
+
+
+def match_capital_gains(sells: list[Sell], buys: list[Buy]):
+    """Matches up sell transactions to buy transactions for capital gains."""
+
+    sell_vol = sum(s.btc for s in sells)
+    buy_vol = sum(b.btc for b in buys)
+
+    assert sell_vol < buy_vol, "Cannot sell more BTC than what was bought."
+
+    cap_gains = []
+
+    for sell in sells:
+        print(sell)
+        pbuys = pop_buys(buys, -sell.btc)
+        print(pbuys)
+        cap_gains += [
+            CapitalGain(b, s) for b, s in zip(pbuys, split_sell(sell, pbuys))
+        ]
+
+    pprint(buys)
+
+    return cap_gains
+
+
+def tabulate(cap_gains: list[CapitalGain]):
+    """Sorts capital gains into long and short durations and by year and
+    formats capital gain information into table rows for reporting.
+    """
+
+    table = {
+        Duration.SHORT: {},
+        Duration.LONG: {},
+    }
+
+    for gain in cap_gains:
+        duration = gain.duration
+        year = gain.sell.timestamp.year
+
+        description = f"{gain.buy.btc:.8f} BTC"
+        date_acquired = gain.buy.timestamp.strftime("%m/%d/%Y")
+        date_sold = gain.sell.timestamp.strftime("%m/%d/%Y")
+        proceeds = f"{gain.sell.usd:.2f}"
+        cost_basis = f"{abs(gain.buy.usd):.2f}"
+        gains = f"{gain.gain:.2f}"
+
+        if not table[duration].get(year):
+            table[duration].update({year: []})
+
+        table[duration][year].append(
+            (description, date_acquired, date_sold, proceeds, cost_basis, gains)
+        )
+
+    return table[Duration.SHORT], table[Duration.LONG]
+
+
+def write_capital_gains(
+    output: Path, short: dict[int, list[tuple]], long: dict[int, list[tuple]]
+):
+    """Writes the capital gains to csv files in the output directory."""
+
+    header = [
+        "Description of Property",
+        "Date Acquired",
+        "Date Sold or Disposed Of",
+        "Proceeds (Sales Price)",
+        "Cost or Other Basis",
+        "Gain or (loss)",
+    ]
+
+    for year, rows in short.items():
+        file_path = output / f"{year}_short_gains.csv"
+        with open(file_path, "w", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            for row in rows:
+                writer.writerow(row)
+
+    for year, rows in long.items():
+        file_path = output / f"{year}_long_gains.csv"
+        with open(file_path, "w", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            for row in rows:
+                writer.writerow(row)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="bcg", description="Bitcoin Capital Gains Calculator"
+    )
+    parser.add_argument(
+        "-i", "--input", required=True, help="Input file directory."
+    )
+    parser.add_argument(
+        "-o", "--output", required=True, help="Output file directory."
+    )
+    args = parser.parse_args()
+
+    input_path = Path(args.input)
+    output_path = Path(args.output)
+
+    gemini_input = read_gemini_input(input_path / "gemini.xlsx")
+    buys = get_buys(gemini_input)
+    sells = get_sells(gemini_input)
+
+    change_strategy(buys, "last_in_first_out")
+
+    cap_gains = match_capital_gains(sells, buys)
+    short, long = tabulate(cap_gains)
+
+    write_capital_gains(output_path, short, long)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 """Tests for bitcoin capital gains calculator."""
 
+import csv
 import shutil
 from datetime import datetime
 from decimal import Decimal
@@ -10,12 +11,19 @@ from pandas import DataFrame
 
 from main import (
     Buy,
+    CapitalGain,
+    Duration,
     Sell,
     Transaction,
     change_strategy,
     get_buys,
     get_sells,
+    match_capital_gains,
+    pop_buys,
     read_gemini_input,
+    split_sell,
+    tabulate,
+    write_capital_gains,
 )
 
 TEST_INPUTS_PATH = Path("tests/input/")
@@ -27,6 +35,28 @@ def test_tx_decimal():
     txn = Transaction(datetime.now(), 1.23, 2.34)
     assert txn.btc == Decimal("1.23")
     assert txn.usd == Decimal("2.34")
+
+
+def test_split_sell_tx():
+    """Should be able to split a sell transaction at a btc amount."""
+
+    timestamp = datetime.now()
+    root = Sell(timestamp, -1, 1)
+    split, remainder = root.split(Decimal("-0.4"))
+
+    assert split == Sell(timestamp, -0.4, 0.4)
+    assert remainder == Sell(timestamp, -0.6, 0.6)
+
+
+def test_split_buy_tx():
+    """Should be able to split a buy transaction at a btc amount."""
+
+    timestamp = datetime.now()
+    root = Buy(timestamp, 1, -1)
+    split, remainder = root.split(Decimal("0.4"))
+
+    assert split == Buy(timestamp, 0.4, -0.4)
+    assert remainder == Buy(timestamp, 0.6, -0.6)
 
 
 def test_sell_tx():
@@ -192,3 +222,300 @@ def test_change_strat_least_expensive_first_out():
         Buy(datetime(2020, 1, 2), 1, -10),
         Buy(datetime(2020, 1, 1), 1, -1),
     ]
+
+
+pop_buy_cases = [
+    pytest.param(
+        [Buy(datetime(2023, 1, 1), 1, -1)],
+        datetime(2023, 6, 1),
+        Decimal("1"),
+        [Buy(datetime(2023, 1, 1), 1, -1)],
+        [],
+        id="single buy",
+    ),
+    pytest.param(
+        [Buy(datetime(2023, 1, 1), 1, -1)],
+        datetime(2023, 6, 1),
+        Decimal("0.4"),
+        [Buy(datetime(2023, 1, 1), 0.4, -0.4)],
+        [Buy(datetime(2023, 1, 1), 0.6, -0.6)],
+        id="single split buy",
+    ),
+    pytest.param(
+        [
+            Buy(datetime(2023, 1, 2), 1, -1),
+            Buy(datetime(2023, 1, 1), 1, -1),
+        ],
+        datetime(2023, 6, 1),
+        Decimal("1.5"),
+        [
+            Buy(datetime(2023, 1, 1), 1, -1),
+            Buy(datetime(2023, 1, 2), 0.5, -0.5),
+        ],
+        [Buy(datetime(2023, 1, 2), 0.5, -0.5)],
+        id="multiple split buy",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "buys,timestamp,btc,expected_popped,expected_buys", pop_buy_cases
+)
+def test_pop_buys(buys, timestamp, btc, expected_popped, expected_buys):
+    """Should pop the correct amount of btc from the buy list."""
+    popped = pop_buys(buys, timestamp, btc)
+    assert popped == expected_popped
+    assert buys == expected_buys
+
+
+def test_split_sell():
+    """Should split a sell transaction according to its matched buys."""
+
+    sell = Sell(datetime(2021, 1, 1), -3, 30)
+    buys = [
+        Buy(datetime(2020, 1, 1), 1, -1),
+        Buy(datetime(2020, 1, 2), 0.5, -0.5),
+        Buy(datetime(2020, 1, 3), 1.5, -1.5),
+    ]
+
+    sells = split_sell(sell, buys)
+
+    assert sells == [
+        Sell(datetime(2021, 1, 1), -1, 10),
+        Sell(datetime(2021, 1, 1), -0.5, 5),
+        Sell(datetime(2021, 1, 1), -1.5, 15),
+    ]
+
+
+@pytest.mark.skip
+def test_match_capital_gains():
+    """Should match up buys to sells as cap gain pairs."""
+
+    buys = [
+        Buy(datetime(2020, 1, 1), 20, -20),
+        Buy(datetime(2021, 1, 1), 10, -10),
+        Buy(datetime(2022, 1, 1), 10, -10),
+    ]
+
+    sells = [
+        Sell(datetime(2020, 6, 1), -5, 50),
+        Sell(datetime(2021, 6, 1), -15, 150),
+        Sell(datetime(2023, 6, 1), -10, 100),
+    ]
+
+    cap_gains = match_capital_gains(sells, buys)
+
+    assert cap_gains == [
+        CapitalGain(
+            Buy(datetime(2020, 1, 1), 5, -5),
+            Sell(datetime(2020, 6, 1), -5, 50),
+        ),
+        CapitalGain(
+            Buy(datetime(2021, 1, 1), 10, -10),
+            Sell(datetime(2021, 6, 1), -10, 100),
+        ),
+        CapitalGain(
+            Buy(datetime(2020, 1, 1), 5, -5),
+            Sell(datetime(2021, 6, 1), -5, 50),
+        ),
+        CapitalGain(
+            Buy(datetime(2022, 1, 1), 10, -10),
+            Sell(datetime(2023, 6, 1), -10, 100),
+        ),
+    ]
+
+    assert buys == [
+        Buy(datetime(2020, 1, 1), 10, -10),
+    ]
+
+
+def test_capital_gain_long_duration():
+    """Should detect if a capital gain is long"""
+
+    cap_gain = CapitalGain(
+        Buy(datetime(2020, 1, 1), 1, -1), Sell(datetime(2021, 6, 1), -1, 10)
+    )
+    assert cap_gain.duration == Duration.LONG
+
+
+def test_capital_gain_short_duration():
+    """Should detect if a capital gain is short"""
+
+    cap_gain = CapitalGain(
+        Buy(datetime(2020, 1, 1), 1, -1), Sell(datetime(2020, 6, 1), -1, 10)
+    )
+    assert cap_gain.duration == Duration.SHORT
+
+
+def test_capital_gain_short_duration_exact_year():
+    """Should detect that a capital gain is short if the duration is exactly
+    one year.
+    """
+
+    cap_gain = CapitalGain(
+        Buy(datetime(2020, 1, 1), 1, -1), Sell(datetime(2021, 1, 1), -1, 10)
+    )
+    assert cap_gain.duration == Duration.SHORT
+
+
+def test_capital_gain_gain():
+    """Should calculate the gain of the capital gain."""
+
+    cap_gain = CapitalGain(
+        Buy(datetime(2020, 1, 1), 1, -1), Sell(datetime(2020, 1, 2), -1, 10)
+    )
+
+    assert cap_gain.gain == 9
+
+
+def test_tabulate():
+    """Should tabulate the capital gains for tax reporting by year."""
+
+    cap_gains = [
+        CapitalGain(
+            Buy(datetime(2020, 1, 1), 1, -1),
+            Sell(datetime(2020, 6, 1), -1, 10),
+        ),
+        CapitalGain(
+            Buy(datetime(2020, 1, 2), 10, -10),
+            Sell(datetime(2021, 6, 1), -10, 200),
+        ),
+        CapitalGain(
+            Buy(datetime(2021, 1, 1), 1.5, -50),
+            Sell(datetime(2021, 6, 1), -1.5, 10),
+        ),
+        CapitalGain(
+            Buy(datetime(2021, 1, 2), 1.12345678, -50),
+            Sell(datetime(2022, 6, 1), -1.12345678, 25),
+        ),
+    ]
+
+    short, long = tabulate(cap_gains)
+
+    assert short == {
+        2020: [
+            (
+                "1.00000000 BTC",
+                "01/01/2020",
+                "06/01/2020",
+                "10.00",
+                "1.00",
+                "9.00",
+            ),
+        ],
+        2021: [
+            (
+                "1.50000000 BTC",
+                "01/01/2021",
+                "06/01/2021",
+                "10.00",
+                "50.00",
+                "-40.00",
+            ),
+        ],
+    }
+
+    assert long == {
+        2021: [
+            (
+                "10.00000000 BTC",
+                "01/02/2020",
+                "06/01/2021",
+                "200.00",
+                "10.00",
+                "190.00",
+            ),
+        ],
+        2022: [
+            (
+                "1.12345678 BTC",
+                "01/02/2021",
+                "06/01/2022",
+                "25.00",
+                "50.00",
+                "-25.00",
+            ),
+        ],
+    }
+
+
+def test_write_capital_gains(tmp_path):
+    """Should write capital gains to csv files."""
+
+    short = {
+        2020: [
+            (
+                "1.00000000 BTC",
+                "01/01/2020",
+                "06/01/2020",
+                "10.00",
+                "1.00",
+                "9.00",
+            ),
+        ],
+        2021: [
+            (
+                "1.50000000 BTC",
+                "01/01/2021",
+                "06/01/2021",
+                "10.00",
+                "50.00",
+                "-40.00",
+            ),
+        ],
+    }
+
+    long = {
+        2021: [
+            (
+                "10.00000000 BTC",
+                "01/02/2020",
+                "06/01/2021",
+                "200.00",
+                "10.00",
+                "190.00",
+            ),
+        ],
+        2022: [
+            (
+                "1.12345678 BTC",
+                "01/02/2021",
+                "06/01/2022",
+                "25.00",
+                "50.00",
+                "-25.00",
+            ),
+        ],
+    }
+
+    write_capital_gains(tmp_path, short, long)
+
+    files = [
+        tmp_path / "2020_short_gains.csv",
+        tmp_path / "2021_short_gains.csv",
+        tmp_path / "2021_long_gains.csv",
+        tmp_path / "2022_long_gains.csv",
+    ]
+
+    for file in files:
+        assert file.exists()
+
+    with open(files[0], "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        assert "Proceeds (Sales Price)" in next(reader)
+        assert "1.00000000 BTC" in next(reader)
+
+    with open(files[1], "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        assert "Proceeds (Sales Price)" in next(reader)
+        assert "1.50000000 BTC" in next(reader)
+
+    with open(files[2], "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        assert "Proceeds (Sales Price)" in next(reader)
+        assert "10.00000000 BTC" in next(reader)
+
+    with open(files[3], "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        assert "Proceeds (Sales Price)" in next(reader)
+        assert "1.12345678 BTC" in next(reader)
